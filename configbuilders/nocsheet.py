b6e2c56c52db5c7d286ce520617e3f026667ad59
Erik Kooistra <me@erikkooistra.nl>
2022-05-05 22:31:53 +0200

    Updated nocsheet to work with sheet api v4

--- configbuilders/nocsheet.py
+++ configbuilders/nocsheet.py
@@ -1,93 +1,82 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import gdata.spreadsheets.client
-#import gdata.spreadsheet.text_db
-import getpass, os, sys
-import pprint
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-def login(source, config, verbose=True):
-  if verbose:
-    print "Connecting to spreadsheet"
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
-  if config.has_option('gdata', 'oauth_token'):
-    token = gdata.gauth.token_from_blob(config.get('gdata', 'oauth_token'))
-  else:
-    SCOPES = ['https://spreadsheets.google.com/feeds/']
-
-    token = gdata.gauth.OAuth2Token(
-      client_id=config.get('gdata', 'oauth_client_id'),
-      client_secret=config.get('gdata', 'oauth_client_secret'),
-      scope=' '.join(SCOPES),
-      user_agent=source)
-
-    print "Visit this URL and get the token:"
-    print
-    print token.generate_authorize_url()
-    print
-    auth_code = raw_input("Enter the code: ")
+# keep backwards compatibility with older scripts passing the login info
+def login(*args, **kwargs):
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    Taken from https://developers.google.com/sheets/api/quickstart/python
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(os.path.expanduser("~/.config/emf/token.json")):
+        creds = Credentials.from_authorized_user_file(
+            os.path.expanduser("~/.config/emf/token.json"), SCOPES
+        )
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                os.path.expanduser("~/.config/emf/credentials.json"), SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(os.path.expanduser("~/.config/emf/token.json"), "w") as token:
+            token.write(creds.to_json())
 
     try:
-      token.get_access_token(auth_code)
-    except gdata.gauth.OAuth2AccessTokenError as err:
-      print "Token failure: " + err.error_message
-      raise
+        service = build("sheets", "v4", credentials=creds)
+        return service
+    except HttpError as err:
+        print(err)
 
-    print
-    print "Add this to /etc/emf-gdata.conf:"
-    print
-    print "oauth_token=" + gdata.gauth.token_to_blob(token)
-    print
-
-    exit(1)
-
-  spr_client = gdata.spreadsheets.client.SpreadsheetsClient(source = source)
-
-  spr_client.ssl = True
-
-  spr_client = token.authorize(spr_client)
-
-  return spr_client
 
 def get_worksheets(spr_client, spreadsheet):
-  worksheets = spr_client.get_worksheets(spreadsheet, None)
-  out = {}
-  for e in worksheets.entry:
-    out[e.title.text] = e.id.text.rsplit('/', 1)[1]
-#  pprint.pprint(out)
-  return out
+    request = spr_client.spreadsheets().get(spreadsheetId=spreadsheet)
+    response = request.execute()
+    worksheets = []
+    for k in response["sheets"]:
+        worksheets.append((k["properties"]["title"]))
+    return worksheets
+
 
 def get_worksheet_data(spr_client, spreadsheet, wks):
-  sheets = get_worksheets(spr_client, spreadsheet)
+    sheets = get_worksheets(spr_client, spreadsheet)
+    if wks not in sheets:
+        print("worksheet " + wks + " not found")
+        exit()
 
-  if wks not in sheets:
-    print "worksheet " + wks + " not found"
-    exit()
+    request = (
+        spr_client.spreadsheets().values().get(spreadsheetId=spreadsheet, range=wks)
+    )
+    response = request.execute()
+    col_keys = {}
+    out = []
 
-  wks_key = sheets[wks]
-  feed = spr_client.get_cells(spreadsheet, wks_key)
-
-  col_keys = {}
-  out = []
-  r = {}
-  cur_row = 1
-  for i, entry in enumerate(feed.entry):
-    row = int(entry.cell.row)
-    col = int(entry.cell.col)
-
-    if row == 1:
-      # assemble keys
-      col_keys[col] = entry.content.text
-      
-    if row != cur_row:
-      if cur_row > 1:
-        out.append(r)
-        if entry.content.text.strip() == "!end":
-          break
-      r = {}
-      cur_row = row
-
-    if entry.content.text != None and entry.content.text != "":
-      r[col_keys[col]] = entry.content.text
-
-#  print len(out)
-  return out
+    for row, entry in enumerate(response["values"]):
+        r = {}
+        if row == 0:
+            for col, col_val in enumerate(entry):
+                col_keys[col] = col_val
+        else:
+            for col, col_val in enumerate(entry):
+                if col_val == "!end":
+                    return out
+                if col_val != None and col_val != "":
+                    r[col_keys[col]] = col_val
+            if r:
+                out.append(r)
+    return out
