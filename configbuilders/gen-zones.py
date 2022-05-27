5148439999f021dbbee7052305653522d1c99ff3
David Croft <david@emfcamp.org>
2022-05-27 19:32:47 +0100

    reverse dns for dhcp leases both vanilla and bizarre

--- configbuilders/gen-zones.py
+++ configbuilders/gen-zones.py
@@ -4,7 +4,7 @@
 # Generate EMF zone files from Netbox
 #
 
-# TODO auto codenames for dhcp sections of dhcp-enabled prefixes
+# TODO some way of making codenames deterministic based on actual IP if we change prefixes during the event
 import argparse
 import getpass
 import ipaddress
@@ -13,6 +13,8 @@ import re
 import shutil
 import sys
 import time
+from ipaddress import _BaseAddress
+from pprint import pprint
 from typing import Dict
 
 import dns.rdata
@@ -27,6 +29,7 @@ import dns.rdtypes.IN.AAAA
 import dns.zone
 import yaml
 from dns import reversename
+from dns.name import Name
 
 from emfnoc import EmfNoc
 from nbh import NetboxHelper
@@ -50,13 +53,29 @@ class ZonesGenerator:
     NS_LIST_HACK_EMF_CAMP = ['ns1.emfcamp.org.', 'A.AUTHNS.BITFOLK.COM.', 'B.AUTHNS.BITFOLK.COM.',
                              'C.AUTHNS.BITFOLK.COM.']
 
-    def __init__(self, helper):
+    codenames: list[str] = []
+    codenamepos: int = 0
+
+    reverse_zones: dict[ipaddress._BaseNetwork, dns.zone.Zone]
+
+    def __init__(self, config, helper):
+
+        self.config = config
+        self.domain_campers = config['dhcpd']['domain_campers']
+        self.domain_orga = config['dhcpd']['domain_orga']
+
         self.helper = helper
 
-    def get_rev_zone(self, address):
+        with open('dns-codenames.txt', 'r') as file:
+            for line in file:
+                line = line.strip().replace(' ', '')
+                if line:
+                    self.codenames.append(line)
+
+    def _get_rev_zone(self, address):
         return reversename.from_address(str(address)).parent()
 
-    def add_soa_and_ns(self, zone):
+    def _add_soa_and_ns(self, zone):
         # Add SOA
         serial = 123  # TODO
         soa_rdataset = zone.find_rdataset('@', dns.rdatatype.SOA, create=True)
@@ -71,62 +90,106 @@ class ZonesGenerator:
             ns_rdataset.add(dns.rdtypes.ANY.NS.NS(dns.rdataclass.IN, dns.rdatatype.NS, ns), self.TTL)
 
     def generate_zones(self):
-        reverse_zones: hash[ipaddress._BaseNetwork, dns.zone.Zone] = {}
+        self.reverse_zones = {}
         aggregates = self.helper.netbox.ipam.aggregates.all()
         for aggregate in aggregates:
             supernet = ipaddress.ip_network(aggregate)
             if supernet.version == 4:
                 for subnet in supernet.subnets(new_prefix=24):
-                    rev_zone_name = str(self.get_rev_zone(subnet.network_address))
+                    rev_zone_name = str(self._get_rev_zone(subnet.network_address))
                     zone = dns.zone.Zone(rev_zone_name)
-                    self.add_soa_and_ns(zone)
+                    self._add_soa_and_ns(zone)
                     self.zones[rev_zone_name] = zone
-                    reverse_zones[subnet] = zone
+                    self.reverse_zones[subnet] = zone
             elif supernet.version == 6:
                 rev_zone_name = str(self.rev_zone_for_ipv6_prefix(supernet))
                 zone = dns.zone.Zone(rev_zone_name)
-                self.add_soa_and_ns(zone)
+                self._add_soa_and_ns(zone)
                 self.zones[rev_zone_name] = zone
-                reverse_zones[supernet] = zone
+                self.reverse_zones[supernet] = zone
 
         for zone_name in self.FWD_DOMAINS:
             zone = dns.zone.Zone(zone_name)
-            self.add_soa_and_ns(zone)
+            self._add_soa_and_ns(zone)
             self.zones[zone_name] = zone
 
         addresses = self.helper.netbox.ipam.ip_addresses.all()
         for nb_address in addresses:
-            name = dns.name.from_text(nb_address.dns_name)
-            address = ipaddress.ip_interface(nb_address.address).ip
+            dns_name = nb_address.dns_name
+            address: _BaseAddress = ipaddress.ip_interface(nb_address.address).ip
+            self._add_host_records(address, dns_name)
 
-            forward_zone = None
-            for zone in self.zones.values():
-                if name.is_subdomain(zone.origin):
-                    forward_zone = zone
+        self._add_dhcp_hostnames()
+        self._add_dns_extras()
 
-            if forward_zone:
-                record_type = dns.rdatatype.A if address.version == 4 else dns.rdatatype.AAAA
-                forward_rdataset = forward_zone.find_rdataset(name, record_type, create=True)
+    def _add_host_records(self, address: _BaseAddress, dns_name: str):
+        name: Name = dns.name.from_text(dns_name)
+        forward_zone = None
+        for zone in self.zones.values():
+            if name.is_subdomain(zone.origin):
+                forward_zone = zone
+        if forward_zone:
+            record_type = dns.rdatatype.A if address.version == 4 else dns.rdatatype.AAAA
+            forward_rdataset = forward_zone.find_rdataset(name, record_type, create=True)
 
-                if address.version == 4:
-                    forward_rdataset.add(dns.rdtypes.IN.A.A(dns.rdataclass.IN, dns.rdatatype.A, str(address)), self.TTL)
-                else:
-                    forward_rdataset.add(dns.rdtypes.IN.AAAA.AAAA(dns.rdataclass.IN, dns.rdatatype.AAAA, str(address)),
-                                         self.TTL)
+            if address.version == 4:
+                forward_rdataset.add(dns.rdtypes.IN.A.A(dns.rdataclass.IN, dns.rdatatype.A, str(address)), self.TTL)
+            else:
+                forward_rdataset.add(dns.rdtypes.IN.AAAA.AAAA(dns.rdataclass.IN, dns.rdatatype.AAAA, str(address)),
+                                     self.TTL)
+        reverse_zone: dns.zone.Zone = None
+        for supernet, zone in self.reverse_zones.items():
+            if address in supernet:
+                reverse_zone = zone
+                break
+        if reverse_zone:
+            rev_name = reversename.from_address(str(address))
 
-            reverse_zone: dns.zone.Zone = None
-            for supernet, zone in reverse_zones.items():
-                if address in supernet:
-                    reverse_zone = zone
-                    break
+            ptr_rdataset = reverse_zone.find_rdataset(rev_name, dns.rdatatype.PTR, create=True)
+            ptr_rdataset.add(dns.rdtypes.ANY.PTR.PTR(dns.rdataclass.IN, dns.rdatatype.PTR, dns_name),
+                             self.TTL)
 
-            if reverse_zone:
-                rev_name = reversename.from_address(str(address))
+    def _add_dhcp_hostnames(self):
+        prefixes = helper.netbox.ipam.prefixes.filter(cf_dhcp=True, family=4)
 
-                ptr_rdataset = reverse_zone.find_rdataset(rev_name, dns.rdatatype.PTR, create=True)
-                ptr_rdataset.add(dns.rdtypes.ANY.PTR.PTR(dns.rdataclass.IN, dns.rdatatype.PTR, nb_address.dns_name),
-                                 self.TTL)
+        for prefix in prefixes:
+            reserved = prefix.dhcp_reserved if 'dhcp_reserved' in prefix else 10
+            network = ipaddress.IPv4Network(prefix.prefix)
+            pool_start = network.network_address + 1 + reserved
+            pool_end = network.broadcast_address - 1
 
+            address = pool_start
+            while address <= pool_end:
+                domain = self.domain_campers if 'Camper-' in prefix.description else self.domain_orga
+
+                fqdn = '%s.%s' % (self._pretty_hostname(address, domain), domain)
+                # TODO this needs to only overwrite the PTR if it doesn't exist.
+                # e.g. if we assign address 63 on video vlan via netbox, that record should take priority
+                # even though the address is part of the dhcp range.
+                # Note: It should still "use up" that codename, so that things following it are not automatically
+                # renumbered!
+                self._add_host_records(address, fqdn)
+                address += 1
+
+    def _pretty_hostname(self, address, domain):
+        if domain == "emf.camp":
+            octets = address.packed
+            return "host-%s-%s-%s-%s" % (str(octets[0]), str(octets[1]), str(octets[2]), str(octets[3]))
+        else:
+            if (self.codenamepos % len(self.codenames) == self.codenamepos / len(self.codenames)):
+                self.codenamepos += 1
+            code1 = self.codenamepos // len(self.codenames)
+            code2 = self.codenamepos % len(self.codenames)
+            if code1 >= len(self.codenames):
+                print("RUN OUT OF self.codenames AT POS %d" % self.codenamepos, file=sys.stderr)
+                exit(1)
+
+            codename1 = self.codenames[code1]
+            codename2 = self.codenames[code2]
+            self.codenamepos += 1
+            return codename1 + "-" + codename2
+
+    def _add_dns_extras(self):
         # Add extras
         with open('dns-extra.yaml', 'r') as f:
             dns_extra = yaml.safe_load(f)
@@ -150,8 +213,7 @@ class ZonesGenerator:
             length -= 8
             pos += 1
 
-        zone_origin = dns.name.from_text('.'.join(reversed(parts)), origin=reversename.ipv6_reverse_domain)
-        return zone_origin
+        return dns.name.from_text('.'.join(reversed(parts)), origin=reversename.ipv6_reverse_domain)
 
     def is_zone_signed(self, zone):
         if os.path.isdir("/etc/bind/signed-zones/%s" % zone):
@@ -238,7 +300,7 @@ if __name__ == "__main__":
     if not os.path.exists('out/zones'):
         os.mkdir('out/zones')
 
-    zonegen = ZonesGenerator(helper)
+    zonegen = ZonesGenerator(config, helper)
 
     print("Generating zones")
 
@@ -262,102 +324,6 @@ if __name__ == "__main__":
         print("zones deployed: %d successfully, %d failed" % (success, failed))
 
 sys.exit(0)
-
-codenamepos = 0
-
-
-def ip6_arpa(octets, therange):
-    out = ""
-    for i in therange:
-        if out != "":
-            out = "." + out
-        out = "%x.%x" % (ord(octets[i]) & 0xf, ord(octets[i]) >> 4) + out
-    return out
-
-
-def reverse_zone(address):
-    octets = address.packed
-    if isinstance(address, ipaddress.IPv4Address):
-        return "%s.%s.%s.in-addr.arpa" % (ord(octets[2]), ord(octets[1]), ord(octets[0]))
-    elif isinstance(address, ipaddress.IPv6Address):
-        # it's a /32, so we want bytes 0-3 (nibbles 0-7)
-        return ip6_arpa(octets, range(0, 4)) + ".ip6.arpa"
-
-
-# x"c.8.0.0.8.f.7.0.1.0.0.2.ip6.arpa"
-# x = ipaddr.IPAddress("2001:7f8:8c:57::11")
-# print reverse_zone(x)
-# print ip6_arpa(x.packed, range(6,16)) # it's a /48 so we want bytes 6-15
-# exit()
-
-def add_record(zonename, record):
-    if zonename not in zones.keys():
-        zones[zonename] = []
-    zones[zonename].append(record)
-
-
-def add_ipv4_host(hostname, fwd_zonename, ipv4, forward_only, reverse_only):
-    if not reverse_only:
-        # v4 forward
-        record = "%s\tIN\tA\t%s" % (hostname, ipv4.compressed)
-        add_record(fwd_zonename, record)
-
-        if hostname in sshfps:
-            for r in sshfps[hostname]:
-                add_record(fwd_zonename, r)
-
-    if forward_only:
-        return
-
-    # v4 reverse
-    rev4_zonename = reverse_zone(ipv4)
-
-    # remove any existing reverse if it exists
-    if rev4_zonename in zones.keys():
-        for record in zones[rev4_zonename]:
-            if record.startswith("%s\tIN\tPTR\t" % (ord(ipv4.packed[3]))):
-                zones[rev4_zonename].remove(record)
-                break
-
-    record = "%s\tIN\tPTR\t%s.%s." % (ord(ipv4.packed[3]), hostname, fwd_zonename)
-    add_record(rev4_zonename, record)
-
-
-def add_ipv6_host(hostname, fwd_zonename, ipv6, forward_only, reverse_only):
-    if not reverse_only:
-        # v6 forward
-        record = "%s\tIN\tAAAA\t%s" % (hostname, ipv6.compressed)
-        add_record(fwd_zonename, record)
-
-    if forward_only:
-        return
-
-    # v6 reverse
-    rev6_zonename = reverse_zone(ipv6)
-    rev6_hostname = ip6_arpa(ipv6.packed, range(4, 16))  # it's a /32 so we want bytes 4-15
-
-    record = "%s\tIN\tPTR\t%s.%s." % (rev6_hostname, hostname, fwd_zonename)
-    add_record(rev6_zonename, record)
-
-
-def pretty_host(zonename, ipv4):
-    if (zonename == "emf.camp"):
-        octets = ipv4.packed
-        return "host-%s-%s-%s-%s" % (ord(octets[0]), ord(octets[1]), ord(octets[2]), ord(octets[3]))
-    else:
-        global codenamepos
-        if (codenamepos % len(codenames) == codenamepos / len(codenames)):
-            codenamepos += 1
-        code1 = codenamepos / len(codenames)
-        code2 = codenamepos % len(codenames)
-        if code1 >= len(codenames):
-            print("RUN OUT OF CODENAMES AT POS %d" % codenamepos)
-            exit(1)
-
-        codename1 = codenames[code1]["Codename"].strip().replace(" ", "")
-        codename2 = codenames[code2]["Codename"].strip().replace(" ", "")
-        codenamepos += 1
-        return codename1 + "-" + codename2
 
 
 def makezonelive(domain, file):
@@ -398,33 +364,6 @@ def reloadzone(domain):
         return True
 
 
-def get_sshfps():
-    #
-    # There is a way of getting puppet to grab the keys
-    # from the hosts it manages and then generating the records
-    # but this was quicker
-    #
-    fps = {}
-    if not os.path.exists("sshfps"):
-        print("no sshfps records")
-        return fps
-
-    fh = open("sshfps", "r")
-    for line in fh:
-        host = line.split()[0]
-        if host not in fps:
-            fps[host] = []
-        fps[host].append(line.strip())
-    fh.close()
-    return fps
-
-
-default_domain = "emf.camp"
-
-config = ConfigParser.ConfigParser()
-if not config.read("/etc/emf-gdata.conf"):
-    print("Warning: config file /etc/emf-gdata.conf could not be found or read")
-
 parser = argparse.ArgumentParser(description='Generate cisco IOS config files from gdocs.')
 
 parser.add_argument('--deploy', action='store_true',
@@ -438,80 +377,7 @@ args = parser.parse_args()
 spreadsheet = config.get('gdata', 'noc_combined')
 
 spr_client = login("emfcamp DNS zone generator", config)
-#
-# print( "downloading Addressing")
-# addressing = get_worksheet_data(spr_client, spreadsheet, "Addressing")
-#
-# print "downloading codenames"
-# codenames = get_worksheet_data(spr_client, spreadsheet, "Codenames")
 
-# for x in range(0, 2560000):
-#  print pretty_host("gchq.org.uk", None)
-
-sshfps = get_sshfps()
-zones = {}
-
-for row in addressing:
-    if "Domain" in row:
-        fwd_zonename = row["Domain"];
-    else:
-        fwd_zonename = default_domain
-
-    # is it a host?
-    if "Hostname" in row and "dns" in row and (row["dns"] == "y" or row["dns"] == "fwd" or row["dns"] == "rev"):
-        hostname = row["Hostname"]
-        if "Subdomain" in row:
-            hostname += "." + row["Subdomain"]
-
-        if "IPv4" in row:
-            ipv4 = ipaddr.IPv4Address(row["IPv4"])
-            add_ipv4_host(hostname, fwd_zonename, ipv4, (row["dns"] == "fwd"), (row["dns"] == "rev"))
-
-        if "IPv6" in row:
-            ipv6 = ipaddr.IPv6Address(row["IPv6"])
-            add_ipv6_host(hostname, fwd_zonename, ipv6, (row["dns"] == "fwd"), (row["dns"] == "rev"))
-
-    # is it a subnet with auto dns?
-    elif "IPv4-Subnet" in row and "dns" in row and row["dns"] == "auto":
-        subnet = ipaddr.IPv4Network(row["IPv4-Subnet"])
-        # router entry
-        if "VLAN" in row:
-            vlan = row["VLAN"]
-            add_ipv4_host("vlan" + vlan + ".ESNORE", "emf.camp", subnet.network + 1, False, False)
-            #      if "IPv6" in row:
-            ipv6 = ipaddr.IPv6Network(row["IPv6"])
-            add_ipv6_host("vlan" + vlan + ".ESNORE", "emf.camp", ipv6.network + 1, False, False)
-
-        # host entries
-        for ipv4 in subnet.iterhosts():
-            if ipv4 == subnet.network + 1:  # first host is the gateway
-                continue
-            hostname = pretty_host(fwd_zonename, ipv4)
-            if "Subdomain" in row:
-                hostname += "." + row["Subdomain"]
-            add_ipv4_host(hostname, fwd_zonename, ipv4, False, False)
-
-    # is it a cname or other fixed record?
-    elif "dns" in row and row["dns"] == "record":
-        hostname = row["Hostname"]
-        if "Subdomain" in row:
-            hostname += "." + row["Subdomain"]
-        add_record(fwd_zonename, hostname + "\t" + row["Description"])
-
-    # if it's a Network with dns='y', we need to 'touch' all the reverse dns to make sure
-    # every individual zone exists
-    elif "Network" in row and "dns" in row and row["dns"] == "y":
-        net = ipaddr.IPv4Network(row["Network"])
-        for subnet in net.iter_subnets(new_prefix=24):
-            zone_name = reverse_zone(subnet)
-            if zone_name not in zones.keys():
-                zones[zone_name] = []
-
-
-# print "ZONES:"
-# pprint.pprint(zones)
-
-# write out zone files
 
 def get_serial(zonename):
     realzone = live_zone_file(zonename)
